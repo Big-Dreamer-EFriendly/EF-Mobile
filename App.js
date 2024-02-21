2fa225796db6bedd2deaee87c3fe81bfc2992194
unknown <Thu Phan@pnvl070.pn.com.vn>
2024-02-21 21:53:35 +0700

    EF-86 Create login screen

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import LoginScreen from './src/screens/login/Login'
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <LoginScreen/>
   )
 }
 
