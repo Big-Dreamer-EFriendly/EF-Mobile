b528d8013bc497c6c1b4c9547544ac61dffd6f36
unknown <Thu Phan@pnvl070.pn.com.vn>
2024-02-22 13:53:12 +0700

    EF-86-Create onboarding screen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import LoginScreen from './src/screens/login/Login'
+import Introduction from './src/screens/introduction/Introduction'
 const App = () => {
   return (
-    <LoginScreen/>
+    // <LoginScreen/>
+    <Introduction/>
   )
 }
 
